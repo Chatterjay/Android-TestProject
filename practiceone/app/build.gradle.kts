b9fb60ab5cf3a1b61fad8cc943a6f7c597b6fff8
Chatter <107738542+Chatterjay@users.noreply.github.com>
2024-10-17 15:24:42 +0800

    集成 QMUI 库并调整主题样式

--- practiceone/app/build.gradle.kts
+++ practiceone/app/build.gradle.kts
@@ -33,12 +33,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.qmuiteam:qmui:2.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- practiceone/app/src/main/java/com/example/practice_one/splash/SplashActivity.kt
+++ practiceone/app/src/main/java/com/example/practice_one/splash/SplashActivity.kt
@@ -3,19 +3,17 @@ package com.example.practice_one.splash
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.practice_one.R
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // 沉浸式状态栏
+        QMUIStatusBarHelper.translucent(this)
+        // 状态栏文字黑色
+        QMUIStatusBarHelper.setStatusBarLightMode(this)
     }
 }
